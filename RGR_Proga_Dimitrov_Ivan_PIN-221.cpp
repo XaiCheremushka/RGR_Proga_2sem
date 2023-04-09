d9d3fe491774c807fb060792f5657bd73de9d5cd
Димитров Иван <124810594+XaiCheremushka@users.noreply.github.com>
2023-04-09 22:16:01 +0600

    Update RGR_Proga_Dimitrov_Ivan_PIN-221.cpp

--- RGR_Proga_Dimitrov_Ivan_PIN-221.cpp
+++ RGR_Proga_Dimitrov_Ivan_PIN-221.cpp
@@ -1,4 +1,4 @@
-﻿#include <windows.h>
+#include <windows.h>
 #include <iostream>
 #include <conio.h>
 #include <string>
@@ -15,7 +15,6 @@ using namespace std;
 #define KPG 17 // предельная точка глобальная
 #define CENTREX 476 // Координаты центра консоли
 #define СENTREY 238 
-
 
 HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -44,46 +43,38 @@ void setcur(int x, int y)//установка курсора на позицию
 };
 
 double proverkaFunc(double y, double x) {
-	if (y <= 10.0 && y >= -10.0) {
+	if (y <= 10.0 && y >= -10.0) 
 		return y;
-	}
-	else {
-		
+	else 
 		return 404.0;
-	}
 }
-
-
 
 
 
 //Анимация меню
 void menuAnim(string Menu[7]) {
 	setlocale(0, "RUS");
-
 	
 	int kPoz = KPG; // предельная точка
 	SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-	
-	//for (int i = size(Menu) - 1; i >= 0; i--) {
+
 	for (int i = 6; i >= 0; i--) {
 		for (int j = 0; j <= kPoz; j++) {
 			Sleep(10);
 			ConsoleCursorVisible();
-			GoToXY(45, j-1);
+			GoToXY(45, j - 1);
 			cout << "                                  " << endl;
 			GoToXY(45, j);
 			cout << Menu[i] << endl;
-			if (kPoz < KPG) 
-				for (int l = KPG; l >= kPoz+1; l--) {
+			if (kPoz < KPG)
+				for (int l = KPG; l >= kPoz + 1; l--) {
 					GoToXY(45, l);
 					//cout << Menu[size(Menu)-(KPG-l)-1] << endl;
-					cout << Menu[7-(KPG-l)-1] << endl;
+					cout << Menu[7 - (KPG - l) - 1] << endl;
 				}
 		}
 		kPoz--;
 	}
-
 }
 
 // Анимация
@@ -91,8 +82,6 @@ void animation() {
 	// Координаты для первой части анимации
 	int coordsX[] = {1, 960, 1, 960};
 	int coordsY[] = { 1, 1, 476, 476 };
-	
-	
 
 	system("cls");
 	HDC screen = GetDC(GetConsoleWindow());
@@ -151,8 +140,6 @@ void animation() {
 	// Вторая часть
 	//int x = CENTREX; int R = CENTREX + 27; int y = sqrt(pow(R, 2) - pow(x, 2));
 	SelectObject(screen, arevWhite);
-	
-
 
 	//variant 2
 	i = 1;
@@ -174,13 +161,11 @@ void animation() {
 
 		if (i % 6 == 0)
 			Sleep(15);
+
 		i++;
 	}
 
-
 	// Третья часть
-	//double y = sqrt(25.00 - (x * x));
-
 	SelectObject(screen, arevCircle);
 	i = 1;
 	double x1 = 0.2;
@@ -229,14 +214,6 @@ void animation() {
 		i++;
 		x1 += 0.2;
 	}
-
-
-
-
-	
-
-
-
 	Sleep(1000);
 }
 
@@ -289,26 +266,377 @@ void authr()
 	_getch();
 }
 
+void table()
+{
+	HANDLE table = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(table, FOREGROUND_BLUE);
+	int i;
+	float F1[20], F2[20], dx, x[20], b = 3.1416, a = -b;
+	dx = fabs(b - a) / 19.0;
+	x[0] = a;
+	SetConsoleTextAttribute(table, FOREGROUND_BLUE | FOREGROUND_GREEN);
+	printf("\n F1 = 2 - cos(x)  F2 = |x + 10|^3 \n");
+	SetConsoleTextAttribute(table,  FOREGROUND_RED);
+	printf("\n Максимальные значения функций F1 и F2 - красный цвет\n");
+	SetConsoleTextAttribute(table, FOREGROUND_GREEN);
+	printf("\n Минимальные значения функций F1 и F2 - зеленый цвет\n");
+	SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+	printf(" _________________________________________\n");
+	printf("|   I   |    X    |    F1    |     F2     |\n");
+	printf("|-------|---------|----------|------------|\n");
+	for (i = 0; i < 20; i++)
+	{
+		F1[i] = 2 - cos(x[i]);
+		F2[i] = pow(abs(x[i] + 10), 3);
+		if (i != 19)
+			x[i + 1] = x[i] + dx;
+		else 
+			x[i] = x[i] + dx;
+	}
+	float max1, max2, min1, min2;
+	max1 = F1[0];
+	min1 = F1[0];
+	max2 = F2[0];
+	min2 = F2[0];
+	for (i = 1; i < 20; i++)
+	{
+		if (F1[i] > max1) max1 = F1[i];
+		if (F1[i] < min1) min1 = F1[i];
+		if (F2[i] > max2) max2 = F2[i];
+		if (F2[i] < min2) min2 = F2[i];
+	}
+	for (i = 0; i < 20; i++)
+	{
+		if (x[i] < 0.0)
+			printf("|  %3d  |  %5.3f |", i + 1, x[i]);
+		else 
+			printf("|  %3d  |   %5.3f |", i + 1, x[i]);
 
+		if (F1[i] == max1)
+		{
+			SetConsoleTextAttribute(table,  FOREGROUND_RED);
+			printf("  %6.3f  ", F1[i]);
+			SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+			printf("|");
+		}
+		else
+		{
+			if (F1[i] == min1)
+			{
+				SetConsoleTextAttribute(table, FOREGROUND_GREEN);
+				printf("  %6.3f  ", F1[i]);
+				SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+				printf("|");
+			}
+			else
+			{
+				SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+				printf("  %6.3f  |", F1[i]);
+			}
+		}
+
+		if (F2[i] == max2)
+		{
+			SetConsoleTextAttribute(table,  FOREGROUND_RED);
+			printf("  %6.3f  ", F2[i]);
+			SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+			printf("|\n");
+		}
+		else
+		{
+			if (F2[i] < 1000)
+				printf(" ");
+			if (F2[i] == min2)
+			{
+				SetConsoleTextAttribute(table, FOREGROUND_GREEN);
+				printf("  %6.3f  ", F2[i]);
+				SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+				printf("|\n");
+			}
+			else
+			{
+				SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+				printf("  %6.3f  |\n", F2[i]);
+			}
+		}
+	}
+	printf("|_______|_________|__________|____________|\n");
+	SetConsoleTextAttribute(table, FOREGROUND_GREEN | FOREGROUND_BLUE);
+	_getch();
+
+	
+}
+
+void graficF2() {
+	system("cls");
+	int pravka;
+
+	HDC screen = GetDC(GetConsoleWindow());
+	HPEN pen_white = CreatePen(PS_DASH, 2, RGB(255, 255, 255));
+	HPEN pen = CreatePen(PS_DASH, 2, RGB(255, 0, 0));
+	HPEN pen2 = CreatePen(PS_DASH, 2, RGB(255, 100, 120));
+	SelectObject(screen, pen_white);
+	//const int CENTREX = 550;
+	//const int CENTREY = 300;
+	// Ось Х
+	MoveToEx(screen, CENTREX - 238, СENTREY, NULL);
+	LineTo(screen, CENTREX + 400, СENTREY);
+	// Ось У
+	MoveToEx(screen, CENTREX, СENTREY - 250, NULL);
+	LineTo(screen, CENTREX, СENTREY + 250);
+	// Стрелочка у оси Х
+	MoveToEx(screen, CENTREX + 400 - 25, СENTREY + 10, NULL);
+	LineTo(screen, CENTREX + 400, СENTREY);
+	LineTo(screen, CENTREX + 400 - 25, СENTREY - 10);
+	// Стрелочка у оси У
+	MoveToEx(screen, CENTREX + 10, СENTREY - 240 + 25, NULL);
+	LineTo(screen, CENTREX, СENTREY - 240);
+	LineTo(screen, CENTREX - 10, СENTREY - 240 + 25);
+	// Палочки
+	for (int i = -238; i <= 360; i += 20) {
+		MoveToEx(screen, CENTREX + i, СENTREY - 5, NULL);
+		LineTo(screen, CENTREX + i, СENTREY + 5);
+	}
+	for (int i = -200; i <= 240; i += 20) {
+		MoveToEx(screen, CENTREX - 5, СENTREY + i, NULL);
+		LineTo(screen, CENTREX + 5, СENTREY + i);
+	}
+	// Подпись осей и начала координат
+	SetBkMode(screen, TRANSPARENT);
+	SetTextColor(screen, RGB(255, 255, 255));
+	TextOut(screen, CENTREX + 15, СENTREY - 230, L"Y", 1);
+	TextOut(screen, CENTREX + 380, СENTREY + 15, L"X", 1);
+	TextOut(screen, CENTREX - 12, СENTREY + 2, L"0", 1);
+	TextOut(screen, CENTREX + 15, СENTREY + 10, L"3", 1);
+	TextOut(screen, CENTREX - 40, СENTREY - 27, L"300", 3);
+	// Подпись кривой функции
+	SetTextColor(screen, RGB(255, 0, 0));
+	TextOut(screen, CENTREX + 200, СENTREY - 110, L"y = |x + 10|^3", 18);
+	// Cам график
+	SelectObject(screen, pen2);
+	int i = 1;
+	for (double x = -20; x < -3.1416; x += 0.01) {
+		MoveToEx(screen, 6.6 * x + CENTREX, -0.066 * pow(abs(x + 10), 3) + СENTREY, NULL);
+		LineTo(screen, 6.6 * (x + 0.01) + CENTREX, -0.066 * pow(abs((x + 0.01) + 10), 3) + СENTREY);
+		i++;
+		if (i % 20 == 0)
+			Sleep(1);
+	}
+	SelectObject(screen, pen);
+	i = 1;
+	for (double x = -3.1416; x <= 3.1416; x += 0.01) {
+		MoveToEx(screen, 6.6 * x + CENTREX, -0.066 * pow(abs(x + 10), 3) + СENTREY, NULL);
+		LineTo(screen, 6.6 * (x + 0.01) + CENTREX, -0.066 * pow(abs((x + 0.01) + 10), 3) + СENTREY);
+		i++;
+		if (i % 20 == 0)
+			Sleep(1);
+	}
+}
+
+void grafik()
+{
+	system("cls");
+	int pravka;
+
+	HDC screen = GetDC(GetConsoleWindow());
+	HPEN pen_white = CreatePen(PS_DASH, 2, RGB(255, 255, 255));
+	HPEN pen = CreatePen(PS_DASH, 2, RGB(80, 170, 220));
+	HPEN pen2 = CreatePen(PS_DASH, 2, RGB(255, 0, 0));
+	SelectObject(screen, pen_white);
+	//const int CENTREX = 550;
+	//const int CENTREY = 300;
+	// Ось Х
+	MoveToEx(screen, CENTREX - 238, СENTREY, NULL);
+	LineTo(screen, CENTREX + 400, СENTREY);
+	// Ось У
+	MoveToEx(screen, CENTREX, СENTREY - 250, NULL);
+	LineTo(screen, CENTREX, СENTREY + 250);
+	// Стрелочка у оси Х
+	MoveToEx(screen, CENTREX + 400 - 25, СENTREY + 10, NULL);
+	LineTo(screen, CENTREX + 400, СENTREY);
+	LineTo(screen, CENTREX + 400 - 25, СENTREY - 10);
+	// Стрелочка у оси У
+	MoveToEx(screen, CENTREX + 10, СENTREY - 240 + 25, NULL);
+	LineTo(screen, CENTREX, СENTREY - 240);
+	LineTo(screen, CENTREX - 10, СENTREY - 240 + 25);
+	// Палочки
+	for (int i = -238; i <= 360; i += 20) {
+		MoveToEx(screen, CENTREX + i, СENTREY - 5, NULL);
+		LineTo(screen, CENTREX + i, СENTREY + 5);
+	}
+	for (int i = -200; i <= 240; i += 20) {
+		MoveToEx(screen, CENTREX - 5, СENTREY + i, NULL);
+		LineTo(screen, CENTREX + 5, СENTREY + i);
+	}
+	// Подпись осей и начала координат
+	SetBkMode(screen, TRANSPARENT);
+	SetTextColor(screen, RGB(255, 255, 255));
+	TextOut(screen, CENTREX + 15, СENTREY - 230, L"Y", 1);
+	TextOut(screen, CENTREX + 380, СENTREY + 15, L"X", 1);
+	TextOut(screen, CENTREX - 12, СENTREY + 2, L"0", 1);
+	TextOut(screen, CENTREX + 15, СENTREY + 10, L"1", 1);
+	TextOut(screen, CENTREX - 15, СENTREY - 27, L"1", 1);
+	// Подпись кривой функции
+	SetTextColor(screen, RGB(80, 170, 220));
+	TextOut(screen, CENTREX + 250, СENTREY - 80, L"y = 2 - cos(x)", 15);
+	SetTextColor(screen, RGB(255, 0, 0));
+	TextOut(screen, CENTREX + 200, СENTREY - 110, L"y = |x + 10|^3", 18);
+	// Cам график
+	int i = 1;
+	SelectObject(screen, pen);
+	for (double x = -3.1416; x <= 3.1416; x += 0.01) {
+		MoveToEx(screen, 20 * x + CENTREX, -20 * (2 - cos(x)) + СENTREY, NULL);
+		LineTo(screen, 20 * (x + 0.01) + CENTREX, -20 * (2 - cos(x + 0.01)) + СENTREY);
+		i++;
+		if (i % 20 == 0)
+			Sleep(1);
+	}
+	_getch();
+	graficF2();
+	_getch();
+}
+
+
+double f1(double x)
+{
+	return 0.5 + cos(x) - 2 * x * sin(x);
+}
+
+
+void uravn()
+{
+	GoToXY(0, 0);
+	double a, b, e = 0.001, x = 0; // e - точность
+	cout << "Уравнение 0.5 + cos(x) - 2 * x * sin(x) = 0." << endl;
+	cout << "Введите начало промежутка » ";
+	cin >> a;
+	cout << "Введите конец промежутка » ";
+	cin >> b;
+	//cout << "Введите точность » ";
+	//cin >> e;
+	if (f1(a) * f1(b) < 0)
+	{
+		while (true)
+		{
+			x = (a + b) / 2;
+			if (fabs(f1(x)) < e) break;
+			if (f1(a) * f1(x) < 0)
+			{
+				a = a;
+				b = x;
+			}
+			else
+			{
+				a = x;
+				b = b;
+			}
+		}
+		cout << "Метод половинного деления" << endl;
+		cout << "Корень x = " << x << endl;
+		while (true)
+		{
+			x = (a * f1(b) - b * f1(a)) / (f1(b) - f1(a));
+			if (fabs(f1(x)) < e) break;
+			if (f1(a) * f1(x) < 0)
+			{
+				a = a;
+				b = x;
+			}
+			else
+			{
+				a = x;
+				b = b;
+			}
+		}
+		cout << "Метод хорд" << endl;
+		cout << "Корень x = " << x << endl;
+	}
+	else
+	{
+		cout << "Условие сходимости не выполнено" << endl;
+	}
+	_getch();
+}
+
+double f(double x)
+{
+	return cos(x) / (sin(x) + cos(x));
+}
+double left_rect(double a, double b, int n) {
+	double h = (b - a) / n;
+	double sum = 0.0;
+	for (int i = 0; i <= n - 1; i++) {
+		sum += h * f(a + i * h);
+	}
+	return sum;
+}
+double right_rect(double a, double b, int n) {
+	double h = (b - a) / n;
+	double sum = 0.0;
+	for (int i = 1; i <= n; i++) {
+		sum += h * f(a + i * h);
+	}
+	return sum;
+}
+double trap(double a, double b, int n) {
+	double h = (b - a) / n;
+	double sum = f(a) + f(b);
+	for (int i = 0; i <= n - 1; i++) {
+		sum += 2 * f(a + i * h);
+	}
+	sum *= h / 2;
+	return sum;
+}
+
+void integral()
+{
+	GoToXY(0,0);
+	double a, b;
+	int n;
+	float h, x, f;
+	printf("Вычисление интеграла cos(x) / (sin(x) + cos(x))\n");
+	cout << "Введите нижний предел интегрирования - ";
+	cin >> a;
+	//a = 1;
+	//b = 2;
+	cout << "\nВведите верхний предел интегрирования - ";
+	cin >> b;
+	//n = 1000;
+	cout << "\nВведите количество частей, на которые следует разделить интервал интегрирования - ";
+	cin >> n;
+	const double eps = 0.001;
+	int k = 10;
+	int i = 0;
+	double diff;
+	do {
+		i++;
+		diff = fabs(left_rect(a, b, k * i) - left_rect(a, b, k * (i + 1)));
+	} while (diff > eps);
+	do {
+		i++;
+		diff = fabs(right_rect(a, b, k * i) - right_rect(a, b, k * (i + 1)));
+	} while (diff > eps);
+	do {
+		i++;
+		diff = fabs(trap(a, b, k * i) - trap(a, b, k * (i + 1)));
+	} while (diff > eps);
+	cout << "Метод левых прямоугольников = " << left_rect(a, b, k * (i + 1)) << "\n";
+	cout << "Метод правых прямоугольников = " << right_rect(a, b, k * (i + 1)) << "\n";
+	cout << "Метод трапеций = " << trap(a, b, k * (i + 1));
+	_getch();
+}
 
 int main()
 {
-	
-
-	//SetConsoleWindowInfo();
-	//string Menu[] = { "0. Графическая заставка","1. Об авторе", "2. Построение таблицы", "3. Построение графиков", "4. Решение уравнения", "5. Вычисление интеграла", "6. Выход" };
 	string Menu[] = { "Графическая заставка","Об авторе", "Построение таблицы", "Построение графиков", "Решение уравнения", "Вычисление интеграла", "Выход" };
 	int activateMenu = 0;
 	char click;
-
 
 	SetConsoleTitle(L"Расчетно-графическая работа");
 	setlocale(0, "RUS");
 	ConsoleCursorVisible();
 
 	menuAnim(Menu);
-	
-	
 
 	while (true) {
 		int x = 45, y = 11;
@@ -354,29 +682,29 @@ int main()
 				system("CLS");
 				GoToXY(x, y);
 				SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-				//table();
+				table();
 				system("CLS");
 				break;
 			case 3:
 				system("CLS");
 				GoToXY(x, y);
 				SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-				//grafik();
+				grafik();
 				system("CLS");
 				break;
 			case 4:
 				system("CLS");
 				GoToXY(x, y);
 				SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-				//uravn();
+				uravn();
 				system("CLS");
 				break;
 			case 5:
 				system("CLS");
 				GoToXY(x, y);
 				SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-				//integral();
-				//_getch();
+				integral();
+				_getch();
 				system("CLS");
 				break;
 			case 6:
@@ -386,11 +714,7 @@ int main()
 		//default:
 		//	cout << "Код " << (int)click << endl;
 		}
-
 	}
-
-
-
 }
 
 /* variant 1
